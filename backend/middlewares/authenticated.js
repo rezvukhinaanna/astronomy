3108334160355607435dae1bdb303f0faa3bfcfa
rezvukhinaanna <rezvukhina.an@yandex.ru>
2025-07-13 02:08:42 +0500

    add docker new 5

--- backend/middlewares/authenticated.js
+++ backend/middlewares/authenticated.js
@@ -1,33 +1,12 @@
 const User = require("../models/User");
 const { verify } = require("../helpers/token");
-const { match } = require("path-to-regexp");
-
-const publicRoutes = [
-  { path: '/register', method: 'POST' },
-  { path: '/login', method: 'POST' },
-  { path: '/products', method: 'GET' },
-  { path: '/products/:id', method: 'GET' },
-];
 
 module.exports = async function (req, res, next) {
   try {
-    // OPTIONS — всегда пропускаем
-    if (req.method === "OPTIONS") return next();
-
-    // Проверяем, попадает ли маршрут в публичные
-    const isPublic = publicRoutes.some(route => {
-      if (route.method !== req.method) return false;
-      const isMatch = match(route.path, { decode: decodeURIComponent });
-      return isMatch(req.path);
-    });
-
-    if (isPublic) return next();
-
-    // Проверка токена
     if (!req.cookies.token) {
       return res.status(401).json({ error: "Токен не предоставлен" });
     }
-
+    
     const tokenData = verify(req.cookies.token);
     const user = await User.findOne({ _id: tokenData.id });
 
